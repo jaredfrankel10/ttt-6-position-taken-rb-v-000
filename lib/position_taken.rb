924bcfe838ad011919cde5bf1b80930af7f58bc0
jared frankel <jaredfrankel10@gmail.com>
2017-09-13 21:40:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 def position_taken?(board, index)
-  if board[index] == "X" or "0"
+  if ["X","O"] == 0
+true
     true
-  elsif ["X","O"] == 0
-  true
+  elsif board[index] == "X" or "0"
+    true
   elsif board[index] == "" or " " or nil
       false
 
